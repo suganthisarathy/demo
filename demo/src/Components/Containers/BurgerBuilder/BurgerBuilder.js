37ef5e0593b5ba5208b063cacb08effba5f18d7b
suganthisarathy <31181665+suganthisarathy@users.noreply.github.com>
2020-04-03 11:53:31 +0530

    Add files via upload

---
+++ demo/src/Components/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react'
+import Aux from '../../hoc/Aux'
+import Burger from '../../../Components/Burger/Burger'
+class BurgerBuilder extends Component{
+  render()
+  {
+      return(<Aux>
+          <Burger/>
+          <div>Build Control</div>
+      </Aux>)
+  }  
+}
+export default BurgerBuilder;
